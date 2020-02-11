b9c6d767b4997994114b3bff8cec617b56495085
101arrowz <arjunbarrett@gmail.com>
2020-02-11 10:29:38 -0800

    Fixed Safari bug

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import MediaRecorderPolyfill from 'audio-recorder-polyfill';
+
+if (!window.MediaRecorder)
+  window.MediaRecorder = MediaRecorderPolyfill;
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./sw.ts');
 }
